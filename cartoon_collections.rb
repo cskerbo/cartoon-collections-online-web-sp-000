097d2777f99f7d3de6d3894c4366ceee9061a246
Chris Kerbo <chris.kerbo@outlook.com>
2019-09-22 21:53:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,14 +5,14 @@ end
 
 def summon_captain_planet(array)
   array.each do |word|
-  
+
 end
 
 def long_planeteer_calls
-  
+
 end
 
 def find_the_cheese
-  
+
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
