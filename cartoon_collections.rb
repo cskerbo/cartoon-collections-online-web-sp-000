aa95365c8e0d118c7ac5b1a9e9c08e609efa262b
Chris Kerbo <chris.kerbo@outlook.com>
2019-09-22 22:12:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,9 +4,9 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-array.each do |word| + "!"
-  word.capitalize
-end
+  array.each do |word|
+    word.capitalize
+  end
 array
 end
 
