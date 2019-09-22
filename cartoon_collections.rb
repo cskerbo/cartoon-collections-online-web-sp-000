ba3daf8ec8f559730655e35f615a6c311f3e7b6d
Chris Kerbo <chris.kerbo@outlook.com>
2019-09-22 23:17:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,8 +23,8 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese_match = array.any? {|cheese| ["cheddar", "gouda", "camembert"].include?(cheese)
-  if cheese_match
+  array.any? {|cheese| ["cheddar", "gouda", "camembert"].include?(cheese)
+  if include?(cheese)
     return cheese
   end}
 end
